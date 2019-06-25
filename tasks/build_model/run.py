bb1deac1689cfb3ef42dd421680e5896434f2b24
Kamil Ciemniewski <kamil@ciemniew.ski>
2019-06-25 08:53:37 +0200

    WIP

--- tasks/build_model/run.py
+++ tasks/build_model/run.py
@@ -1,13 +1,50 @@
+import pandas as pd
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 from glob import glob
+import os
 
 
 def build_model():
-    files = glob('/data/blog/**/**/**/*.md')
+    articles = pd.read_parquet('/data/articles.parquet')
+    tagged_data = [
+            TaggedDocument(words=word_tokenize(_d.lower()), tags=[articles['file'][i]])
+            for i, _d in enumerate(articles['text'])
+    ]
 
-    print(f"{files}")
-    pass
+    max_epochs = 100
+    vec_size = 25
+    alpha = 0.025
+
+    model = Doc2Vec(size=vec_size,
+                    alpha=alpha,
+                    workers=8,
+                    negative=10,
+                    min_alpha=0.00025,
+                    min_count=1,
+                    dm=1)
+
+    model.build_vocab(tagged_data)
+
+    for epoch in range(max_epochs):
+        if epoch % 10 == 0:
+            print('epoch {0}'.format(epoch))
+
+        model.train(tagged_data,
+                    total_examples=model.corpus_count,
+                    epochs=model.iter)
+
+        # decrease the learning rate
+        model.alpha -= 0.0002
+
+        # fix the learning rate, no decay
+        model.min_alpha = model.alpha
+
+    model.save("/data/articles.model")
+    print("Model Saved")
 
 if __name__ == '__main__':
-    build_model()
+    if os.path.isfile('/data/articles.model'):
+        print("Skipping as the model file already exsists")
+    else:
+        build_model()
---
+++ tasks/preprocess/run.py
@@ -0,0 +1,41 @@
+import re
+import io
+import os
+from glob import glob
+import pandas as pd
+
+
+def read_file(path):
+    result = ""
+    with io.open(path, "r", encoding="utf-8") as file:
+        result = file.read()
+    return result
+
+def prune_front_matter(text):
+    lines = text.split("\n")
+    seen = 0
+
+    for ix, line in enumerate(lines):
+        if line == '---':
+            if seen == 1:
+                return "\n".join(lines[(ix+1):]).strip()
+            else:
+                seen += 1
+
+def remove_code(text):
+    return re.sub(r'==##==', "\n", re.sub(r'```.*```', '' , re.sub(r'\n', '==##==', text)))
+
+def build_dataframe():
+    files = glob('/data/blog/**/**/**/*.md')
+    contents = [read_file(path) for path in files]
+    texts = [remove_code(prune_front_matter(text)).lower() for text in contents]
+
+    articles = pd.DataFrame({ 'file': ["/".join(p.split("/")[2:]) for p in files], 'text': texts })
+    articles.to_parquet('/data/articles.parquet')
+
+if __name__ == '__main__':
+    if os.path.isfile('/data/articles.parquet'):
+        print("Skipping as the data frame file already exsists")
+    else:
+        build_dataframe()
+
